93dd4076f04f7ec3ee96a829b2a7033f72b53b48
Thibault Savenkoff <thibault.savenkoff@outlook.fr>
2024-08-26 21:03:02 +0200

    Forgot to git add, lol!

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,37 @@
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
+    <main>
+      <div className="text-center p-52">
+        <h1 className="font-bold">
+          <p className="text-5xl">Hi!</p>
+          <p className="text-5xl">Ready To Study?</p>
+        </h1>
+        <div className="mt-4">
+          <a href="/docs" className="inline-block px-5 py-3 rounded-lg shadow-lg white:bg-slate-300 dark:bg-slate-600">Connect</a>
+        </div>
+      </div>
+
+      <div className="absolute bottom-4 right-4 flex items-start">
         <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
+          className="pointer-events-auto lg:flex gap-2"
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+          <p className="sm:text-pw lg:text-base">Powered by{" "}</p>
+          <Image
+              src="/next.svg"
+              alt="Next.js Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+          />
+        </Link>
+      </div>
     </main>
   );
 }
